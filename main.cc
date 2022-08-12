8fa73c3b3f1e36847df6c279639efddc2a1e34e4
Unno Hideyuki <unno.hideyuki@gmail.com>
2022-08-13 08:04:08 +0900

    added cassert and modint

--- main.cc
+++ main.cc
@@ -1,10 +1,16 @@
 #include <atcoder/all>
 using namespace atcoder;
 #include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
 using ll = long long;
 const double pi = 3.14159265359;
 const ll INF = 1LL << 60;
+
+//using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;  // mint::set_mod(p); later
+//using mint = static_modint<1000000009>;
 
 int main()
 {
