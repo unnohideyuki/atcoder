f19366113a3c56231558e67d14e8897264739a29
Unno Hideyuki <unno.hideyuki@gmail.com>
2022-08-22 20:47:16 +0900

    inserted chmin/chmax

--- main.cc
+++ main.cc
@@ -7,6 +7,9 @@ using ll = long long;
 const double pi = 3.14159265359;
 const ll INF = 1LL << 60;
 
+template<class T> inline bool chmin(T& a, T b){ if (a > b){a = b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b){ if (a < b){a = b; return true;} return false;}
+
 //using mint = modint998244353;
 //using mint = modint1000000007;
 //using mint = modint;  // mint::set_mod(p); later
